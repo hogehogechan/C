445298d3aa1398568081808eed9be9d0f254cdd6
hogehogechan <sho.kobayashi0927@gmail.com>
2016-10-23 23:40:46 +0900

    first

--- temperature/main.c
+++ temperature/main.c
@@ -8,8 +8,11 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+/* 摂氏、華氏の対応表を印字する */
+int main()
+{
+    int fahr;
+    
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
