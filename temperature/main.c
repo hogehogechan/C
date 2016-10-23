445e91f222b623efe4c78145210bcc6a86121aa3
hogehogechan <sho.kobayashi0927@gmail.com>
2016-10-23 23:34:02 +0900

    Initial Commit

---
+++ temperature/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  temperature
+//
+//  Created by USER on 2016/10/23.
+//  Copyright © 2016年 USERtesttest. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
