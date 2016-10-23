de16a1358867ccf124ae47a6a4847e5853e64c95
hogehogechan <sho.kobayashi0927@gmail.com>
2016-10-24 00:28:42 +0900

    降順にソート

--- temperature/main.c
+++ temperature/main.c
@@ -12,7 +12,8 @@
 int main()
 {
     int fahr;
+    int n = 300;
     
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (fahr = n; fahr >= 0; fahr = fahr - 20)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
